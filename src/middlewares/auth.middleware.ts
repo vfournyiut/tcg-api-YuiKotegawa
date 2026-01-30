a90cfcc49d6c7268a50931fe3853e25afb4bda04
Senechal_Scott <100836619+roux2scours@users.noreply.github.com>
2026-01-30 09:34:15 +0100

    modification des types

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,8 +1,9 @@
-import {Request, Response, NextFunction} from "express";
+import {Response, NextFunction} from "express";
 import jwt from "jsonwebtoken";
 import {env} from "../env";
+import { JWTRequest } from "../types/auth.types";
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+export const authenticateToken = (req: JWTRequest, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
     const token = authHeader?.split(" ")[1];
 
@@ -18,3 +19,4 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
         return res.status(401).json({error: "Invalid or expired token"});
     }
 };
+
